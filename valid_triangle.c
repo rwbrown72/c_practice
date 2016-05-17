8b02674e9f0bb1424e65b8149191ffe5af9b95c0
Russell Brown <russellbrowncompany@gmail.com>
2016-05-17 16:59:15 -0500

    functions to add

---
+++ valid_triangle.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int valid_triangle(int a, int b, int c);
+
+int main(int argc, string argv[])
+{	
+	int a;
+	int b;
+	int c;
+
+	printf("Is your triangle valid?\n");
+
+	do 
+	{
+		printf("What is the length of side \"a\" of the triangle: \n");
+		a = GetInt();
+		printf("What is the length of side \"b\" of the triangle: \n");
+		b = GetInt();
+		printf("What is the length of side \"c\" of the triangle: \n");
+		c = GetInt();
+
+	} while (a < 1 || b < 1 || c < 1);
+
+	int valid_triangle(int a, int b, int c);
+
+	if (valid_triangle > 0)
+	{
+		printf("Your Triangle is valid.\n");
+	}
+	else 
+	{
+		printf("Your Triangle is invalid. \n");
+	}
+}
+
+int valid_triangle(int a, int b, int c)
+{
+	if ( (a+b) >= c && (a+c) >= b && (b+c) >=a)
+	{
+		printf("returning 1\n");
+		return 1;
+	}
+	else 
+	{
+		printf("returning 0\n");
+		return 0;
+	}
+}
